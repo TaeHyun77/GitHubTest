52d387b83ff328c3d2112eb658ee430087992fa1
TaeHyun77 <acd2283@gmail.com>
2026-02-22 03:34:38 +0900

    first commit

---
+++ src/test/kotlin/com/example/GitHupTest/GitHupTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.GitHupTest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GitHupTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
